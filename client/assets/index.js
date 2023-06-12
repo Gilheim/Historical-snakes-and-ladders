310ae90051309f4aaf20c92a1a94e2d3c880ea70
Hussein Raji <111huraji@gmail.com>
2023-06-12 20:56:33 +0100

    created throwdice function

--- client/assets/index.js
+++ client/assets/index.js
@@ -1,4 +1,6 @@
 let players = [];
+// a value from 0 to 3 dictating which player is next to throw
+let currentPlayer = 0;
 
 const createPlayers = () => {
   const formQueryString = window.location.search;
@@ -19,4 +21,40 @@ const createPlayers = () => {
 
 createPlayers();
 
-console.log(players);
+// console.log(players);
+
+const diceButton = document.getElementById("dice");
+const diceThrowInfo = document.getElementById("dice-throw-info");
+
+const throwDice = async () => {
+  const randValue1to6 = Math.ceil(Math.random() * 6);
+
+  diceButton.classList.add("dice-throw");
+  diceButton.disabled = true;
+  diceThrowInfo.dataset.thrown = true;
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  //change dice image mid-throw
+  diceButton.innerHTML = "";
+  const image = document.createElement("img");
+  image.src = `./assets/images/dice/${randValue1to6}.svg`;
+  image.alt = `value of ${randValue1to6} on die`;
+  diceButton.appendChild(image);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  diceButton.classList.remove("dice-throw");
+  diceButton.disabled = false;
+  diceThrowInfo.dataset.thrown = false;
+
+  diceThrowInfo.textContent = `"${players[currentPlayer].name}" threw a value of ${randValue1to6}`;
+
+  //update current players position on the grid using random value
+  const playerObj = players[currentPlayer];
+  playerObj.currentSquare += randValue1to6;
+
+  // updateBoard()
+};
+
+diceButton.addEventListener("click", throwDice);
