d6deef34b374a964f167d8d7efbffab0e4527863
Hussein Raji <111huraji@gmail.com>
2023-06-13 09:25:26 +0100

    created player creation and move piece functions in js file

--- client/assets/index.js
+++ client/assets/index.js
@@ -21,6 +21,49 @@ const createPlayers = () => {
 
 createPlayers();
 
+const boardGrid = document.getElementById("board-grid");
+
+/**
+ * Takes the id of a player piece and a square and places piece centered within square
+ *
+ * @param {string} pieceId - id of the div representing a player playing
+ * @param {string} squareElem - id of the div representing a square within the board
+ */
+const movePiece = (pieceId, squareId) => {
+  const pieceElem = document.getElementById(pieceId);
+  const squareElem = document.getElementById(squareId);
+
+  const squareCenterPosInGrid = {};
+  squareCenterPosInGrid.xPos =
+    squareElem.offsetLeft + squareElem.offsetWidth / 2;
+  squareCenterPosInGrid.yPos =
+    squareElem.offsetTop + squareElem.offsetHeight / 2;
+
+  pieceElem.style.left =
+    squareCenterPosInGrid.xPos - pieceElem.offsetWidth / 2 + "px";
+  pieceElem.style.top =
+    squareCenterPosInGrid.yPos - pieceElem.offsetHeight / 2 + "px";
+};
+
+const createPlayerHTMLElements = () => {
+  for (let i = 0; i < players.length; i++) {
+    const playerId = `player${i}`;
+    const playerObj = players[i];
+
+    playerObj.id = playerId;
+    const playerDiv = document.createElement("div");
+    playerDiv.id = playerId;
+
+    playerDiv.classList.add("board-piece");
+
+    boardGrid.appendChild(playerDiv);
+
+    movePiece(playerId, "square-1");
+  }
+};
+
+createPlayerHTMLElements();
+
 // console.log(players);
 
 const diceButton = document.getElementById("dice");
@@ -54,7 +97,13 @@ const throwDice = async () => {
   const playerObj = players[currentPlayer];
   playerObj.currentSquare += randValue1to6;
 
-  // updateBoard()
+  // use recently updated current square of player to move player piece to the correct position on the board
+  const pieceId = playerObj.id;
+  const squareId = `square-${playerObj.currentSquare}`;
+  movePiece(pieceId, squareId)
+
+  //update the current player next to throw the dice
+  currentPlayer = (currentPlayer + 1) % players.length;
 };
 
 diceButton.addEventListener("click", throwDice);
